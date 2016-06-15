93b17bac56a78da9f5a24660d11e906aa043ae51
max <max.teh.lueser@gmail.com>
2016-06-15 15:10:02 +0100

    Account for single/double digits in minutes

--- src/ui_helper.js
+++ src/ui_helper.js
@@ -4,10 +4,14 @@
 	
 	// Generate timestamp for post, formatted like this: M/DD/YYYY HH:MM:SS AM/PM
 	var dateObject = new Date();
+	
 	var date = (dateObject.getMonth() + 1) + "/" + dateObject.getDate() + "/" + dateObject.getFullYear();
-	var time = dateObject.getHours() + ":" + dateObject.getMinutes() + ":" 
+	
+	var time = dateObject.getHours() + ":" 
 			// Account for single/double digits
+			+ ((dateObject.getMinutes() < 10) ? "0" + dateObject.getMinutes() : dateObject.getMinutes());
 			+ ((dateObject.getSeconds() < 10) ? "0" + dateObject.getSeconds() : dateObject.getSeconds());
+			
 	var meridiem = (dateObject.getHours() < 12) ? "AM" : "PM";
 	
 	timestamp.innerHTML = date + " " + time + " " + meridiem;
