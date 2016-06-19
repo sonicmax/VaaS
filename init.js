a108c9fd158d77e84d7aea47e6e1056985a96ebd
max <max.teh.lueser@gmail.com>
2016-06-19 20:57:10 +0100

    Add logging

--- init.js
+++ init.js
@@ -206,6 +206,7 @@ app.getTopicList = function() {
 			
 			var $ = cheerio.load(response);
 			var topicList = $('.grid tbody tr').get();
+			console.log("topic list length:", topicList.length);
 			// Pick random topic to pester. The -1 / +1 here looks goofy, but is required to make sure we don't get 0 as output
 			var target = topicList.get[Math.floor((Math.random() * topicList.length - 1)) + 1];
 			var topicHref = target('td a').get[0].attr('href');
