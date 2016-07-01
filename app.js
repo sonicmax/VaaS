1607c66017184a03b6971792b167b65cac822f27
max <max.teh.lueser@gmail.com>
2016-07-01 02:44:58 +0100

    Check that environment vars are configured correctly before connecting to db

--- app.js
+++ app.js
@@ -27,34 +27,46 @@ var routes = require("./routes/routes.js")(app);
 // Set up server
 var server = app.listen(process.env.PORT || LOCAL_HOST, () => {
 	
-	// TODO: maybe we could check process.env here - to avoid error handling later on
+	// Check that required environment variables exist
+	if (!process.env.USERNAME || !process.env.PASSWORD || !process.env.USER_ID || !process.env.REDIS_URL || !process.env.TOKEN) {
+		console.log("ERROR: environment variables not configured correctly. see README for more info");
+		return;
+	}
 	
-	// Connect to db and retrieve quotes.
-	app.db = client.createClient(process.env.REDIS_URL);
+	// Environment vars are always strings, so we need to check that USER_ID can be converted to integer.
+	if (typeof parseInt(process.env.USER_ID, 10) !== "number") {
+		console.log("ERROR: USER_ID couldn't be parsed to integer, doublecheck your settings");
+		return;
+	}
 
-	app.db.on("connect", () => {
-		
-		app.db.lrange("quotes", 0, -1, (error, items) => {
+	else {
+		// Connect to db and retrieve quotes.
+		app.db = client.createClient(process.env.REDIS_URL);
+
+		app.db.on("connect", () => {
 			
-			if (error) {	
-				console.log(process.env.USERNAME + "-bot init failed to load db. check that redis is configured correctly");
-				return;
-			}
-			
-			else if (items.length === 0) {
-				console.log(process.env.USERNAME + "-bot init success, but db was empty");
-				return;
-			}
-					
-			else {
-				// Pregenerate some text for first user
-				markovChain.setInput(items);
-				markovChain.generate(app, false, null);
-				console.log(process.env.USERNAME + "-bot init success");				
-			}
+			app.db.lrange("quotes", 0, -1, (error, items) => {
+				
+				if (error) {	
+					console.log(process.env.USERNAME + "-bot init failed to load db. check that redis is configured correctly");
+					return;
+				}
+				
+				else if (items.length === 0) {
+					console.log(process.env.USERNAME + "-bot init success, but db was empty");
+					return;
+				}
+						
+				else {
+					// Pregenerate some text for first user
+					markovChain.setInput(items);
+					markovChain.generate(app, false, null);
+					console.log(process.env.USERNAME + "-bot init success");				
+				}
+				
+			});
 			
 		});
-		
-	});
+	}
 	
 });
